b9e5f519e353ca1bbc580e05494d4f44244ef472
rahul080327 <rahul080327@gmail.com>
2015-11-22 14:33:28 +0530

    Rahul | Added home redirect to bare URLs

--- agilecalendar/urls.py
+++ agilecalendar/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    url(r'^$', lambda r: HttpResponseRedirect('agcal/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^agcal/', include('agcal.urls'))
 ]
