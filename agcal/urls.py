007e8c75acaf9f8667e6feb5eb5e0304d8186431
rahul080327 <rahul080327@gmail.com>
2015-12-10 14:18:29 +0530

    Rahul | Upgraded url patterns for Django 1.10

--- agcal/urls.py
+++ agcal/urls.py
@@ -1,18 +1,19 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from agcal import views
 
-urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
-                       url(r'^api/login/(?P<username>[\w\-]+)/$', views.login),
-                       url(r'^api/login/(?P<username>[\w\-]+)$', views.login),
-                       url(r'^api/logout/(?P<username>[\w\-]+)/$', views.logout),
-                       url(r'^api/logout/(?P<username>[\w\-]+)$', views.logout),
-                       url(r'^api/user/(?P<username>[\w\-]+)/$', views.user),
-                       url(r'^api/user/(?P<username>[\w\-]+)$', views.user),
-                       url(r'^api/user/(?P<username>[\w\-]+)/boards$', views.boards),
-                       url(r'^api/user/(?P<username>[\w\-]+)/boards/$', views.boards),
-                       url(r'^api/user/(?P<username>[\w\-]+)/board/(?P<board_id>[\w\-]+)$', views.board),
-                       url(r'^api/user/(?P<username>[\w\-]+)/board/(?P<board_id>[\w\-]+)/$', views.board),
-                       url(r'^api/user/(?P<username>[\w\-]+)/board$', views.board),
-                       url(r'^api/user/(?P<username>[\w\-]+)/board/$', views.board))
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^api/login/(?P<username>[\w\-]+)/$', views.login),
+    url(r'^api/login/(?P<username>[\w\-]+)$', views.login),
+    url(r'^api/logout/(?P<username>[\w\-]+)/$', views.logout),
+    url(r'^api/logout/(?P<username>[\w\-]+)$', views.logout),
+    url(r'^api/user/(?P<username>[\w\-]+)/$', views.user),
+    url(r'^api/user/(?P<username>[\w\-]+)$', views.user),
+    url(r'^api/user/(?P<username>[\w\-]+)/boards$', views.boards),
+    url(r'^api/user/(?P<username>[\w\-]+)/boards/$', views.boards),
+    url(r'^api/user/(?P<username>[\w\-]+)/board/(?P<board_id>[\w\-]+)$', views.board),
+    url(r'^api/user/(?P<username>[\w\-]+)/board/(?P<board_id>[\w\-]+)/$', views.board),
+    url(r'^api/user/(?P<username>[\w\-]+)/board$', views.board),
+    url(r'^api/user/(?P<username>[\w\-]+)/board/$', views.board)
+]
