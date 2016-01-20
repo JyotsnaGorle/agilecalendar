2dfbeec791a89e2297cd231beb1c367adff10878
rahul080327 <rahul080327@gmail.com>
2016-01-20 13:37:36 +0530

    Rahul | Added test for GET /api/user/:username

--- README.md
+++ README.md
@@ -39,14 +39,14 @@ https://nodejs.org/en/download/
 
 - Clone this repository
 - In the repo directory, run ``` npm install ```
-- Run migrations using ``` sequelize --env production|test|[development] db:migrate ```
+- Run migrations using ``` sequelize --env production|[development] db:migrate ```
 
 ### For development 
 - Run ``` gulp ``` in the root folder.
 - Will start as ``` node --harmony index --db development ``` with file watchers on 8000
 
 ### Others
-- run the app using ``` node --harmony index --db production|test|[development] ``` on port 8000
+- run the app using ``` node --harmony index --db production|[development] ``` on port 8000
 
 ## Running Tests
 - Run ``` gulp test ```
--- test/endpoints.js
+++ test/endpoints.js
@@ -2,12 +2,49 @@ var app = require('../index.js').app;
 var request = require('co-supertest').agent(app.listen(8000));
 var expect = require('chai').expect;
 
+var User = require('../utils/model_getter').get('user');
+var flinkCrypto = require('../utils/flink_crypto');
+
 describe('GET /api/status', function () {
     it('should return status as {"status":"Flink says Hallo!"}', function* () {
         var res = yield request.get('/api/status').expect(200).end();
 
         expect(JSON.parse(res.text)).to.deep.equal({
             status: "Flink says Hallo!"
+        });
+    });
+});
+
+describe('GET /api/user/:username', function () {
+    beforeEach(function() {
+        User.destroy({
+            where: {
+                username: 'heisenberg'
+            }
+        }).then(function () {
+            User.create({
+                name: 'Walter White',
+                email: 'imtheonewhoknocks@amc.com',
+                username: 'heisenberg',
+                password: flinkCrypto.hash('saymyname')
+            });
+        });
+    });
+
+    it('should get user details for a given username', function* () {
+        var res = yield request.get('/api/user/heisenberg').expect(200).end();
+
+        expect(JSON.parse(res.text)).to.deep.equal({
+            name: "Walter White",
+            email: "imtheonewhoknocks@amc.com"
+        });
+    });
+
+    it('should respond 404 for a non existent username', function* () {
+        var res = yield request.get('/api/user/non_user').expect(404).end();
+
+        expect(JSON.parse(res.text)).to.deep.equal({
+            response: "No such user found"
         });
     });
 });
